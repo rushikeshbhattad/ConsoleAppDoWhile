bfc446db86c5907dc2f0812925a72d21ace8a469
Rushikesh Bhattad <rushikeshbhattad8@gmail.com>
2021-08-30 15:28:51 +0530

    Add project files.

---
+++ ConsoleAppDoWhile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DoWhile
+{
+    class Also
+    {
+        public void Check(int n)
+        {
+            int i = 0;
+            int j = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            } while (i <= n);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Also a = new Also();
+            Console.Write("Enter The Value of N : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            a.Check(num);
+            Console.ReadKey();
+        }
+    }
+}
